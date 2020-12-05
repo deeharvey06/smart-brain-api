a63fbdda187c89117bfeafc623be3137ba1c79ed
Clifford Harvey <charvey@mychirotouch.com>
2020-12-05 15:06:15 -0800

    fix req bug

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.get('/', (req, res) => {res.send('it is working!') });
 
 app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt));
 
-app.post('/register', (req, res) => register.handleRegister(res, req, db, bcrypt) );
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt) );
 
-app.get('/profile/:id', (req, res) => profile.handleProfileGet(res, req, db) );
+app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db) );
 
-app.put('/image', (req, res) =>  image.handlerImageUpdate(res, req, db) );
+app.put('/image', (req, res) =>  image.handlerImageUpdate(req, res, db) );
 
 app.post('/imageurl', image.handlerApiCall);
 
